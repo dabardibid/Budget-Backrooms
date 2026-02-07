3f47b60d9b350ede18775e25b2aee45983accf43
DavidJoacaRo <32200281+DavidJoacaRo@users.noreply.github.com>
2026-02-07 01:04:36 +0100

    [*] Fix engine bug "CreateExport: Failed to load ..."
    Weird ass engine bug, all i had to do is put the actors back in /Actors/, fix up references from /Actors/Lights/ and then move them back from /Actors/ in ./Lights/ like wtf??

--- Source/BudgetBackrooms/BBHashThing.h
+++ Source/BudgetBackrooms/BBHashThing.h
@@ -1,3 +1,3 @@
 #pragma once
-#define PROJECT_GIT_HASH "914fc6cb"
-#define PROJECT_GIT_HASH_LONG "914fc6cbf9c22bd5a9d78338dc2c7b11b518c049"
+#define PROJECT_GIT_HASH "d5d9358b"
+#define PROJECT_GIT_HASH_LONG "d5d9358be2e9dd1dbb7c6b7b627b37ce31921367"
